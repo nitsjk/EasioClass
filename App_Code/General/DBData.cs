c38e4a94babf300bb971618400069bbb32bc0780
nitsjk <nitsjandk@gmail.com>
2020-06-16 11:12:37 -0700

    Gerenal & Ustc update

---
+++ App_Code/General/DBData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DBData
+/// </summary>
+/// 
+namespace Nits
+{
+    public class DBData
+    {
+        // 1 for local sever and  2 for online server
+        public static int UTCDate = 1;
+    }
+}
